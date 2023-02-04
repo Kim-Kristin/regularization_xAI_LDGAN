f2fbf8a2ade96ec7cad05e4fe268268f2c3469fb
Kim-Kristin <582494@fom-net.de>
2023-02-04 11:22:41 +0100

    add interactive main

--- model/main.py
+++ model/main.py
@@ -1,5 +1,10 @@
+#Import runtime libraries
+import os
+import sys
+
 # Module - Training DCGAN
 
+import torch
 from inceptionnetwork import InceptionV3
 import inceptionnetwork
 import weightinit
@@ -11,6 +16,7 @@ from discriminator import DiscriminatorNetCifar10 as NN_Discriminator
 from FID import CalcFID
 from train import train_GAN
 import sys
+import plot
 # Append needed function/module paths
 sys.path.append('./model')
 sys.path.append('./model/train')
@@ -23,9 +29,13 @@ sys.path.append('./src/weightinit')
 sys.path.append('./src/device')
 sys.path.append('./src/dataloader')
 sys.path.append('./src/param')
+sys.path.append('./src/plot')
+#Support function for clearing terminal output
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
-#GAN - Generator and Discriminator
+#GAN
 NN_Generator = NN_Generator().to(device.device)
 NN_Discriminator = NN_Discriminator().to(device.device)
 
@@ -34,25 +44,112 @@ block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[2048]
 model = InceptionV3([block_idx])
 model = model.to(device.device)
 
-# DCGAN
-#G_loss, D_loss, FID = train_GAN.train_DCGAN(NN_Discriminator, NN_Generator, model , dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial)
 
-#xG_loss, xD_loss = training.train_xAIGAN(NN_Discriminator, NN_Generator, weightinit.w_initial, dataloader.train_loader, True, param.num_epochs, param.random_Tensor, param.lr, param.device)
+#Project main
+def project_main ():
+    #GAN - Generator and Discriminator
 
-#ldG_loss, ldD_loss = training.train_LDGAN(NN_Discriminator, NN_Generator, weightinit.w_initial, dataloader.train_loader, True, param.num_epochs, param.random_Tensor, param.lr, param.device)
+    print("##########")
+    print("Regularisierung von tiefen Neuronalen Netzen zur Bildgenerierung mittels xAI:Evaluierung der Effektivität von eXplainable LDGAN gegenüber State-of-the-Art Regularisierungsmethoden​")
+    print("##########")
+    print("\n")
+    print("Regularisierungs-Model")
+    print("0. All Models")
+    print("1. Vanilla DCGAN")
+    print("2. Gradient Penalty - GAN")
+    print("3. Weight Normalization with Clipping GAN")
+    print("4. Weight Penalty - GAN")
+    print("5. Imbalanced GAN")
+    print("6. Layeroutput Normalization")
+    print("7. GAN with different Losses for D and G")
+    print("8. xAI GAN")
+    print("9. LDGAN")
 
-#ClipG_loss, clippD_loss = training.train_GAN_with_WP_Normalization(NN_Discriminator, NN_Generator,  dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial, param.choice_gc_gs, start_idx=1)
 
-# Weight Penalty GAN
-#G_loss_WP, D_loss_WP, FID_WP = train_GAN.train_GAN_with_WP(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial, param.g_features, param.latent_size)
+    user_input = int(input("Regularisierungs-Model:"))
+    if user_input == 0:
+        cls()
+        G_loss, D_loss, FID, FID_test = train_GAN.train_DCGAN(NN_Discriminator, NN_Generator, model , dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial)
+        GPG_loss, GPD_loss, GP_FID, FID_test_gp = train_GAN.train_GAN_with_GP(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial)
+        ClipG_loss, ClipD_loss, ClipFID, FID_test_clip = train_GAN.train_GAN_with_WP_Normalization(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial, param.choice_gc_gs, start_idx=1)
+        G_loss_WP, D_loss_WP, FID_WP, FID_test_WP = train_GAN.train_GAN_with_WP(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial, param.g_features, param.latent_size)
+        G_loss_IT, D_loss_IT, FID_IT, FID_test_IT = train_GAN.train_GAN_Imbalanced(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial, param.N_CRTIC)
+        G_loss_diffLoss, D_loss_diffLoss, FID_diffLoss, FID_test_diff = train_GAN.train_GAN_with_diffrent_Losses(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial)
+        G_loss_LN, D_loss_LN, FID_LN, FID_test_LN = train_GAN.train_GAN_Normalization(NN_Generator, NN_Discriminator, model, dataloader.train_loader,  weightinit.w_initial, param.lr, param.num_epochs, param.batch_size, param.random_Tensor, device.device)
+        ldG_loss, ldD_loss, FID_ldgan, FID_test_ldgan= train_GAN.train_LDGAN(NN_Discriminator, NN_Generator, model, weightinit.w_initial, dataloader.train_loader, True, param.num_epochs, param.random_Tensor, param.lr, param.device)
 
 
-'''Normalization'''
-G_loss_Norm, D_loss_Norm, FID_Norm = train_GAN.train_GAN_Normalization(NN_Generator, NN_Discriminator, model, dataloader.train_loader, weightinit.w_initial, param.lr, param.num_epochs, param.batch_size, param.random_Tensor, device.device)
+    elif user_input == 1:
+        cls()
+        #DCGAN
+        G_loss, D_loss, FID, FID_test = train_GAN.train_DCGAN(NN_Discriminator, NN_Generator, model , dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial)
 
-'''Different Losses'''
-#G_loss_DiffLoss, D_loss_DiffLoss, FID_DiffLoss = train_GAN.train_GAN_with_diffrent_Losses(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial)
+    elif user_input == 2:
+        cls()
+        #Gradient Penalty
 
-#testDCGAN = FID("./state_save/DCGAN.tar", NN_Generator, dataloader.test_loader, device.device, param.random_Tensor)
+        GPG_loss, GPD_loss, GP_FID, FID_test_gp = train_GAN.train_GAN_with_GP(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, weightinit.w_initial)
 
-# G_losses, D_losses =train_DCGAN.training(NN_Discriminator, NN_Generator, param.limited, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial, True)
+    elif user_input == 3:
+        cls()
+        #Clipping
+        ClipG_loss, ClipD_loss, ClipFID, FID_test_clip = train_GAN.train_GAN_with_WP_Normalization(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial, param.choice_gc_gs)
+
+    elif user_input == 4:
+        cls()
+        #Weight Penalty
+        G_loss_WP, D_loss_WP, FID_WP, FID_test_WP = train_GAN.train_GAN_with_WP(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial, param.g_features, param.latent_size)
+
+    elif user_input==5:
+        cls()
+        #Imbalanced Training
+        G_loss_IT, D_loss_IT, FID_IT, FID_test_IT = train_GAN.train_GAN_Imbalanced(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial, param.N_CRTIC)
+    elif user_input == 6:
+        cls()
+        #Layer Output Normalization
+        G_loss_LN, D_loss_LN, FID_LN, FID_test_LN = train_GAN.train_GAN_Normalization(NN_Generator, NN_Discriminator, model, dataloader.train_loader,  weightinit.w_initial, param.lr, param.num_epochs, param.batch_size, param.random_Tensor, device.device)
+
+    elif user_input == 7:
+        cls()
+        #GAN with Diffrent Losses for D and G
+        G_loss_diffLoss, D_loss_diffLoss, FID_diffLoss, FID_test_diff = train_GAN.train_GAN_with_diffrent_Losses(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial)
+    elif user_input == 8:
+        cls()
+        # GAN with xAI
+        #xG_loss, xD_loss, FID_xAI, FID_test_xAI = train_GAN.train_xAIGAN(NN_Discriminator, NN_Generator, model, weightinit.w_initial, dataloader.train_loader, True, param.num_epochs, param.random_Tensor, param.lr, param.device)
+
+    elif user_input == 9:
+        cls()
+        # LDGAN
+        ldG_loss, ldD_loss, FID_ldgan, FID_test_ldgan= train_GAN.train_LDGAN(NN_Discriminator, NN_Generator, model, weightinit.w_initial, dataloader.train_loader, True, param.num_epochs, param.random_Tensor, param.lr, device.device)
+
+    elif user_input == 10:
+        cls()
+        print("### State of Metrics ###")
+        print("### Vanilla DCGAN ###")
+        output = torch.load("./outputs/metrics/_Van_GAN_Checkpoint.pth")
+
+        print("### Gradient Penalty ###")
+        output = torch.load("./outputs/metrics/_Van_GAN_Checkpoint.pth")
+
+        print("### Weight Clipping ###")
+        output = torch.load("./outputs/metrics/_Van_GAN_Checkpoint.pth")
+
+        print("### Weight Penalty ###")
+        output = torch.load("./outputs/metrics/_Van_GAN_Checkpoint.pth")
+
+        print("### Layer Output Normalization ###")
+        output = torch.load("./outputs/metrics/_Van_GAN_Checkpoint.pth")
+
+        print("### Imbalanced Training ###")
+        output = torch.load("./outputs/metrics/_Van_GAN_Checkpoint.pth")
+
+        print("### Different Losses for D and G ###")
+        output = torch.load("./outputs/metrics/_Van_GAN_Checkpoint.pth")
+
+        print("### LDGAN ###")
+        output = torch.load("./outputs/metrics/_Van_GAN_Checkpoint.pth")
+
+project_main()
+
+
