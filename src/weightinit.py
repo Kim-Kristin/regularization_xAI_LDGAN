87db8bef7783ff858bcc4ba27e540f72d5105b93
Kim-Kristin <582494@fom-net.de>
2023-02-07 19:18:12 +0100

    change params in weight inital

--- src/weightinit.py
+++ src/weightinit.py
@@ -4,9 +4,17 @@ import torch.nn as nn
 # mean 0 and Standardabweichung 0.02
 def w_initial(m):
     """ initialize convolutional and batch norm layers in generator and discriminator """
-    classname = m.__class__.__name__
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        torch.nn.init.normal_(m.weight, mean=0.0, std=0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        torch.nn.init.normal_(m.weight, mean=0.0, std=0.02)
+        torch.nn.init.constant_(m.bias, val=0)
+
+
+    """classname = m.__class__.__name__
     if classname.find('Conv') != -1:
         nn.init.normal_(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
+"""
