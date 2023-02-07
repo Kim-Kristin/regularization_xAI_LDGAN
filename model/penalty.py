88271dcf9cf2ac10d872ba56d86dcf3894b091e9
Kim-Kristin <582494@fom-net.de>
2023-02-07 18:43:38 +0100

    Add a separt Function for GAN GP

--- model/penalty.py
+++ model/penalty.py
@@ -4,7 +4,39 @@ import torch
 def compute_gradient_penalty(D, real_samples, fake_samples, device):
     """Calculates the gradient penalty loss for WGAN GP"""
     # Random weight term for interpolation between real and fake samples
-    alpha = torch.rand(real_samples.size(0), 1, 1, 1).to(device)
+    #alpha = torch.rand(real_samples.size(0), 1, 1, 1).to(device)
+    batch_size, channel, height, width= real_samples.shape
+
+    # alpha radomisiert zwischen 0 und 1 gewählt
+    alpha= torch.rand(batch_size,1,1,1).repeat(1, channel, height, width).to(device)
+    # Get random interpolation between real and fake samples
+    interpolates = (alpha * real_samples + ((1 - alpha) * fake_samples)).requires_grad_(True).to(device)
+    d_interpolates = D(interpolates, GAN_param=2).to(device)
+    #print(d_interpolates.shape)
+    fake = torch.ones(real_samples.size(0), 1).requires_grad_(False)
+    fake = torch.flatten(fake)
+    #print(fake.shape)
+    # Get gradient w.r.t. interpolates
+    gradients = torch.autograd.grad(
+        outputs=d_interpolates,
+        inputs=interpolates,
+        grad_outputs=fake,
+        create_graph=True,
+        retain_graph=True
+        #only_inputs=True
+    )[0]
+    gradients = gradients.view(gradients.size(0), -1)
+    gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+    return gradient_penalty
+
+def compute_GAN_gradient_penalty(D, real_samples, fake_samples, device):
+    """Calculates the gradient penalty loss for GAN GP"""
+    # Random weight term for interpolation between real and fake samples
+    #alpha = torch.rand(real_samples.size(0), 1, 1, 1).to(device)
+    batch_size, channel, height, width= real_samples.shape
+
+    # alpha radomisiert zwischen 0 und 1 gewählt
+    alpha= torch.rand(batch_size,1,1,1).repeat(1, channel, height, width).to(device)
     # Get random interpolation between real and fake samples
     interpolates = (alpha * real_samples + ((1 - alpha) * fake_samples)).requires_grad_(True).to(device)
     d_interpolates = D(interpolates, GAN_param=0).to(device)
@@ -18,9 +50,10 @@ def compute_gradient_penalty(D, real_samples, fake_samples, device):
         inputs=interpolates,
         grad_outputs=fake,
         create_graph=True,
-        retain_graph=True,
-        only_inputs=True,
+        retain_graph=True
+        #only_inputs=True
     )[0]
     gradients = gradients.view(gradients.size(0), -1)
     gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
     return gradient_penalty
+
