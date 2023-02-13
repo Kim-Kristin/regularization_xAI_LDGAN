7d02f659e4b1d0eade8779df54e7714cb3cb6c2e
Kim-Kristin <582494@fom-net.de>
2023-02-13 13:07:10 +0100

    Set Comments and modification function call

--- model/main.py
+++ model/main.py
@@ -36,11 +36,11 @@ def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-#GAN
+#GAN - Generator and Discriminator
 NN_Generator = NN_Generator().to(device.device)
 NN_Discriminator = NN_Discriminator().to(device.device)
 
-# FID - InceptionV3
+# call InceptionV3 for FID Score
 block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[2048]
 model = InceptionV3([block_idx])
 model = model.to(device.device)
@@ -48,7 +48,6 @@ model = model.to(device.device)
 
 #Project main
 def project_main ():
-    #GAN - Generator and Discriminator
 
     print("##########")
     print("Regularisierung von tiefen Neuronalen Netzen zur Bildgenerierung mittels xAI:Evaluierung der Effektivität von eXplainable LDGAN gegenüber State-of-the-Art Regularisierungsmethoden​")
@@ -57,17 +56,14 @@ def project_main ():
     print("Regularisierungs-Model")
     print("0. All Models")
     print("1. Vanilla DCGAN")
-    print("2. Gradient Penalty - GAN")
-    print("3. Weight Normalization with Clipping GAN")
-    print("4. Weight Penalty - GAN")
-    print("5. Imbalanced GAN")
-    print("6. Layeroutput Normalization")
-    print("7. GAN with different Losses for D and G")
-    print("8. xAI GAN")
-    print("9. LDGAN")
+    print("2. Gradient Penalty ")
+    print("3. Weight Normalization with Clipping GAN (WGAN)")
+    print("4. Imbalanced GAN (WGAN-GP)")
+    print("5. Layer-Output Normalization (Instance Normalization)")
+    print("6. xAI-LDGAN")
 
     print("##########")
-    print("10. Call Checkpoints (Metrics - Losses and FID) of all Models")
+    print("7. Call Checkpoints (Metrics - Losses and FID) of all Models")
     print("##########")
 
     user_input = int(input("Regularisierungs-Model:"))
@@ -76,12 +72,9 @@ def project_main ():
         G_loss, D_loss, FID, FID_test = train_GAN.train_DCGAN(NN_Discriminator, NN_Generator, model , dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial)
         GPG_loss, GPD_loss, GP_FID, FID_test_gp = train_GAN.train_GAN_with_GP(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, weightinit.w_initial)
         ClipG_loss, ClipD_loss, ClipFID, FID_test_clip = train_GAN.train_GAN_with_WP_Normalization(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, weightinit.w_initial, param.N_CRTIC)
-        #G_loss_WP, D_loss_WP, FID_WP, FID_test_WP = train_GAN.train_GAN_with_WP(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial, param.g_features, param.latent_size)
         G_loss_IT, D_loss_IT, FID_IT, FID_test_IT = train_GAN.train_GAN_Imbalanced(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial, param.N_CRTIC)
-        #G_loss_diffLoss, D_loss_diffLoss, FID_diffLoss, FID_test_diff = train_GAN.train_GAN_with_diffrent_Losses(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial)
         G_loss_LN, D_loss_LN, FID_LN, FID_test_LN = train_GAN.train_GAN_Normalization(NN_Generator, NN_Discriminator, model, dataloader.train_loader,  weightinit.w_initial, param.lr, param.num_epochs, param.batch_size, param.random_Tensor, device.device)
         ldG_loss, ldD_loss, FID_ldgan, FID_test_ldgan= train_GAN.train_LDGAN(NN_Discriminator, NN_Generator, model, weightinit.w_initial, dataloader.train_loader, True, param.num_epochs, param.random_Tensor, param.lr, device.device)
-
 
     elif user_input == 1:
         cls()
@@ -91,7 +84,6 @@ def project_main ():
     elif user_input == 2:
         cls()
         #Gradient Penalty
-
         GPG_loss, GPD_loss, GP_FID, FID_test_gp = train_GAN.train_GAN_with_GP(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, weightinit.w_initial)
 
     elif user_input == 3:
@@ -99,35 +91,22 @@ def project_main ():
         #Clipping
         ClipG_loss, ClipD_loss, ClipFID, FID_test_clip = train_GAN.train_GAN_with_WP_Normalization(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, weightinit.w_initial, param.N_CRTIC)
 
-    elif user_input == 4:
-        cls()
-        #Weight Penalty
-        #G_loss_WP, D_loss_WP, FID_WP, FID_test_WP = train_GAN.train_GAN_with_WP(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial, param.g_features, param.latent_size)
-
-    elif user_input==5:
+    elif user_input==4:
         cls()
         #Imbalanced Training
         G_loss_IT, D_loss_IT, FID_IT, FID_test_IT = train_GAN.train_GAN_Imbalanced(NN_Discriminator, NN_Generator, model, dataloader.train_loader, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial, param.N_CRTIC)
-    elif user_input == 6:
+
+    elif user_input == 5:
         cls()
         #Layer Output Normalization
         G_loss_LN, D_loss_LN, FID_LN, FID_test_LN = train_GAN.train_GAN_Normalization(NN_Generator, NN_Discriminator, model, dataloader.train_loader,  weightinit.w_initial, param.lr, param.num_epochs, param.batch_size, param.random_Tensor, device.device)
 
-    elif user_input == 7:
-        cls()
-        #GAN with Diffrent Losses for D and G
-        G_loss_diffLoss, D_loss_diffLoss, FID_diffLoss, FID_test_diff = train_GAN.train_GAN_with_diffrent_Losses(NN_Discriminator, NN_Generator, model, dataloader.train_dataloader_CIFAR10, param.random_Tensor, param.num_epochs, device.device, param.lr, param.batch_size, weightinit.w_initial)
-    elif user_input == 8:
-        cls()
-        # GAN with xAI
-        #xG_loss, xD_loss, FID_xAI, FID_test_xAI = train_GAN.train_xAIGAN(NN_Discriminator, NN_Generator, model, weightinit.w_initial, dataloader.train_loader, True, param.num_epochs, param.random_Tensor, param.lr, param.device)
-
-    elif user_input == 9:
+    elif user_input == 6:
         cls()
         # LDGAN
         ldG_loss, ldD_loss, FID_ldgan, FID_test_ldgan= train_GAN.train_LDGAN(NN_Discriminator, NN_Generator, model, weightinit.w_initial, dataloader.train_loader, True, param.num_epochs, param.random_Tensor, param.lr, device.device)
 
-    elif user_input == 10:
+    elif user_input == 7:
         cls()
         print("### !!!MAKE SURE THAT TRAINING STATE IS SAVED!!! - State of Metrics ###")
         print("### Vanilla DCGAN ###")
@@ -138,27 +117,19 @@ def project_main ():
         output = torch.load("./outputs/metrics/_GP_GAN_Checkpoint.pth")
         print(output)
 
-        print("### Weight Clipping ###")
-        output = torch.load("./outputs/metrics/_Clipping_GAN_Checkpoint.pth") #, map_location='cpu')
+        print("### Weight Clipping (WGAN) ###")
+        output = torch.load("./outputs/metrics/_Clipping_GAN_Checkpoint.pth")
         print(output)
 
-        #print("### Weight Penalty ###")
-        #output = torch.load("./outputs/metrics/_WP_GAN_Checkpoint.pth")
-        #print(output)
-
-        print("### Imbalanced Training ###")
+        print("### Imbalanced Training (WGAN-GP) ###")
         output = torch.load("./outputs/metrics/_IT_GAN_Checkpoint.pth")
         print(output)
 
-        print("### Layer Output Normalization ###")
+        print("### Layer-Output Normalization (Instance Normalization) ###")
         output = torch.load("./outputs/metrics/_LN_GAN_Checkpoint.pth")
         print(output)
 
-        print("### Different Losses for D and G ###")
-        output = torch.load("./outputs/metrics/_DiffLoss_GAN_Checkpoint.pth")
-        print(output)
-
-        print("### LDGAN ###")
+        print("### xAI-LDGAN ###")
         output = torch.load("./outputs/metrics/_LDGAN_Checkpoint.pth")
         print(output)
 
