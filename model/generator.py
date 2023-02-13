66dc98842ef9270b47f0e9adffb73aba255f1062
Kim-Kristin <582494@fom-net.de>
2023-02-13 13:06:22 +0100

    Chance batch to instance normalizaton in G

--- model/generator.py
+++ model/generator.py
@@ -36,19 +36,19 @@ class GeneratorNetworkCIFAR10(torch.nn.Module):
         self.input_layer = nn.Sequential(
             # input is Z, going into a convolution
             nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(ngf * 8),
+            nn.InstanceNorm2d(ngf * 8),
             nn.ReLU(True),
             # state size. (ngf*8) x 4 x 4
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 4),
+            nn.InstanceNorm2d(ngf * 4),
             nn.ReLU(True),
             # state size. (ngf*4) x 8 x 8
             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 2),
+            nn.InstanceNorm2d(ngf * 2),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
             nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf),
+            nn.InstanceNorm2d(ngf),
             nn.ReLU(True))
 
         self.out = nn.Sequential(
